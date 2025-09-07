1d075fbc15dfd453788382e14b8d5af7a0698383
Namsai Supavong <namsaisupavong@Namsais-MacBook-Pro.local>
2025-09-07 21:03:31 +0100

    viewport

--- docs/generate_sandwiches.py
+++ docs/generate_sandwiches.py
@@ -120,6 +120,7 @@ def main():
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Master Degree of Sandwich Making</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
